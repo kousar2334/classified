d4e31775f6925951abc81591b78903f828da7b95
kousar2334 <kousar.cse2334@gmail.com>
2026-02-24 21:00:50 +0600

    modify header

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,144 +1,83 @@
 <footer>
-    <div class="footerWrapper footerStyleOne plr">
+    <div class="footerWrapper footerStyleOne">
+
+        {{-- ── Main footer columns ── --}}
         <div class="footer-area footer-padding">
-            <div class="container-fluid">
-                <div class="row justify-content-between">
-                    <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-6 col-sm-6">
-                        <div class="footer-widget widget  mb-24">
-                            <div class="footer-tittle">
+            <div class="container">
+                <div class="row gy-4">
 
-                                <div class="footer-logo mb-40">
-                                    <a href="index.html" class="footer-logo">
-                                        <h3 class="text-white"> {{ get_setting('site_name') }}</h3>
-                                    </a>
-                                </div>
-                                <ul class="listing">
-                                    <li class="listItem wow fadeInUp" data-wow-delay="0.0s"><a href="index.html#"
-                                            class="singleLinks"> <i class="las la-map-marker icon"></i> Dhaka,
-                                            Bangladesh</a></li>
-                                    <li class="listItem wow fadeInUp" data-wow-delay="0.0s"> <a href="index.html#"
-                                            class="singleLinks"> <i class="las la-phone-square icon"></i>
-                                            +627-521-1504</a></li>
-                                    <li class="listItem wow fadeInUp" data-wow-delay="0.0s"> <a href="index.html#"
-                                            class="singleLinks"> <i class="las la-eye icon"></i> <span
-                                                class="__cf_email__"
-                                                data-cfemail="03707673736c7177436f6a70776c6066626d2d606c6e">[email&#160;protected]</span></a>
-                                    </li>
-                                </ul>
-                                <div class="footer-social2">
-                                    <a class="wow fadeInUp social" data-wow-delay="0.2s" href="index.html"> <i
-                                            class=""></i> </a>
-
-                                </div>
-                            </div>
+                    {{-- Brand + Contact --}}
+                    <div class="col-lg-4 col-md-6">
+                        <div class="footer-brand">
+                            <h3 class="footer-site-name">{{ get_setting('site_name') }}</h3>
+                            <p class="footer-tagline">Find the best deals near you.</p>
+                            <ul class="footer-contact-list">
+                                <li>
+                                    <i class="las la-map-marker"></i>
+                                    Dhaka, Bangladesh
+                                </li>
+                                <li>
+                                    <i class="las la-phone-square"></i>
+                                    +627-521-1504
+                                </li>
+                                <li>
+                                    <i class="las la-envelope"></i>
+                                    info@example.com
+                                </li>
+                            </ul>
                         </div>
                     </div>
-                    <div class="col-xxl-2 col-xl-2 col-lg-6 col-md-6 col-sm-6">
-                        <div class="footer-widget widget  mb-24">
-                            <div class="footer-tittle">
-                                <h6 class="footerTittle">Categories</h6>
-                                <ul class="listing">
-                                    <li class="listItem wow fadeInUp" data-wow-delay="0.0s"><a class="singleLinks"
-                                            href="listing/category/property.html">Education</a></li>
-                                    <li class="listItem wow fadeInUp" data-wow-delay="0.0s"><a class="singleLinks"
-                                            href="listing/category/appliances.html">Appliances</a></li>
-                                    <li class="listItem wow fadeInUp" data-wow-delay="0.0s"><a class="singleLinks"
-                                            href="listing/category/pets-and-animals.html">Pet’s & Animals</a></li>
-                                    <li class="listItem wow fadeInUp" data-wow-delay="0.0s"><a class="singleLinks"
-                                            href="listing/category/books--sports--hobbies.html">Sports</a></li>
-                                    <li class="listItem wow fadeInUp" data-wow-delay="0.0s"><a class="singleLinks"
-                                            href="listing/category/fashion.html">Fashion</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xxl-2 col-xl-2 col-lg-6 col-md-6 col-sm-6">
-                        <div class="footer-widget widget  mb-24">
-                            <div class="footer-tittle">
-                                <h6 class="footerTittle">About</h6>
-                                <ul class="listing">
-                                    <li class="listItem wow fadeInUp" data-wow-delay="0.0s"><a href="about.html"
-                                            class="singleLinks"> About Us</a></li>
-                                    <li class="listItem wow fadeInUp" data-wow-delay="0.0s"><a href="blog.html"
-                                            class="singleLinks"> Blog</a></li>
-                                    <li class="listItem wow fadeInUp" data-wow-delay="0.0s"><a
-                                            href="terms-and-conditions.html" class="singleLinks"> Terms &
-                                            Conditions</a></li>
-                                    <li class="listItem wow fadeInUp" data-wow-delay="0.0s"><a
-                                            href="privacy-policy.html" class="singleLinks"> Privacy Policy</a></li>
 
-                                </ul>
-                            </div>
-                        </div>
+                    {{-- Categories --}}
+                    <div class="col-lg-3 col-md-3 col-6">
+                        <h6 class="footerTittle">Categories</h6>
+                        <ul class="footer-links">
+                            <li><a href="#">Education</a></li>
+                            <li><a href="#">Appliances</a></li>
+                            <li><a href="#">Pets &amp; Animals</a></li>
+                            <li><a href="#">Sports</a></li>
+                            <li><a href="#">Fashion</a></li>
+                        </ul>
                     </div>
-                    <div class="col-xxl-2 col-xl-2 col-lg-6 col-md-6 col-sm-6">
-                        <div class="footer-widget widget  mb-24">
-                            <div class="footer-tittle">
-                                <h6 class="footerTittle">Help &amp; support</h6>
-                                <ul class="listing">
-                                    <li class="listItem wow fadeInUp" data-wow-delay="0.0s"><a href="contact.html"
-                                            class="singleLinks"> Contact</a></li>
-                                    <li class="listItem wow fadeInUp" data-wow-delay="0.0s"><a href="faq.html"
-                                            class="singleLinks"> Faq</a></li>
-                                    <li class="listItem wow fadeInUp" data-wow-delay="0.0s"><a
-                                            href="safety-informations.html" class="singleLinks"> Safety
-                                            Informations</a></li>
 
-                                </ul>
-                            </div>
-                        </div>
+                    {{-- About --}}
+                    <div class="col-lg-3 col-md-3 col-6">
+                        <h6 class="footerTittle">About</h6>
+                        <ul class="footer-links">
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Terms &amp; Conditions</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                        </ul>
                     </div>
-                    <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-6 col-sm-6">
-                        <div class="footer-widget widget  mb-24">
-                            <div class="footer-tittle">
-                                <h6 class="footerTittle">Newsletter</h6>
-                                <div class="footer-pera footer-pera2">
-                                    <p class="pera wow fadeInUp">Be the first one to know news, offers and events
-                                        weekly in your. Unsubscribe whenever you like with one click.</p>
-                                </div>
-                                <div class="footer-form mt-10 wow fadeInRight">
-                                    <div class="form-row mb-20">
-                                        <form action="https://listocean.bytesed.com/news-letter/subscribe/by/user"
-                                            class="newsletter-footer" method="post"
-                                            id="newsletter_subscribe_from_addon">
-                                            <input class="input" type="email" name="email"
-                                                placeholder="Your Email Address">
-                                            <div class="error-message mt-2"></div>
-                                            <div class="btn-wrapper form-icon">
-                                                <button
-                                                    class="btn-default btn-rounded subscription_by_email">Submit</button>
-                                            </div>
-                                        </form>
-                                    </div>
-                                    <div class="footer-social2 ">
-                                        <a class="wow fadeInUp social" data-wow-delay="0.2s" href="fb.com.html"> <i
-                                                class="lab la-facebook-f"></i> </a>
-                                        <a class="wow fadeInUp social" data-wow-delay="0.2s"
-                                            href="https://twitter.com">
-                                            <i class="lab la-twitter"></i> </a>
 
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    {{-- Help & Support --}}
+                    <div class="col-lg-2 col-md-3 col-6">
+                        <h6 class="footerTittle">Help &amp; Support</h6>
+                        <ul class="footer-links">
+                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">FAQ</a></li>
+                            <li><a href="#">Safety Info</a></li>
+                        </ul>
                     </div>
+
                 </div>
             </div>
         </div>
-        <!-- footer-bottom area -->
+
+        {{-- ── Copyright bar ── --}}
         <div class="footer-bottom-area">
-            <div class="container-fluid">
+            <div class="container">
                 <div class="footer-border">
-                    <div class="row">
-                        <div class="col-xl-12">
-                            <div class="footer-copy-right text-center">
-                                <p class="pera wow fadeInDown" data-wow-delay="0.0s">All copyright © 2025 Reserved
-                                </p>
-                            </div>
-                        </div>
+                    <div class="footer-copy-right text-center">
+                        <p class="pera">
+                            All copyright &copy; {{ date('Y') }} {{ get_setting('site_name') }}. All Rights
+                            Reserved.
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
 </footer>
