7b63e9290fa4ddcf5c84e318591f1924e1e6fbca
kousar2334 <kousar.cse2334@gmail.com>
2026-03-10 00:13:46 +0600

    modify safety tips edit page

--- resources/views/backend/modules/safety-tips/edit.blade.php
+++ resources/views/backend/modules/safety-tips/edit.blade.php
@@ -26,18 +26,21 @@
                 <div class="row">
                     <div class="col-lg-9 col-12">
                         <div class="card">
-                            <ul class="nav nav-tabs nav-fill border-light border-0">
-                                @foreach (getAllLanguages() as $key => $language)
-                                    <li class="nav-item">
-                                        <a class="nav-link @if ($language->code == $lang) active border-0 @else bg-light @endif py-3"
-                                            href="{{ route('classified.settings.safety.tips.edit', ['id' => $tips->id, 'lang' => $language->code]) }}">
-                                            <img src="{{ asset('/public/web-assets/backend/img/flags') . '/' . $language->code . '.png' }}"
-                                                width="20px">
-                                            <span>{{ $language->name }}</span>
+                            <div class="lang-switcher-wrap mb-0">
+                                <div class="lang-switcher-label">
+                                    <i class="fas fa-globe-americas"></i>
+                                    <span>{{ translation('Language') }}</span>
+                                </div>
+                                <div class="lang-switcher-tabs">
+                                    @foreach (getAllLanguages() as $key => $language)
+                                        <a href="{{ route('classified.settings.safety.tips.edit', ['id' => $tips->id, 'lang' => $language->code]) }}"
+                                            class="lang-switcher-btn @if ($language->code == $lang) active @endif">
+                                            <span class="lang-dot"></span>
+                                            {{ $language->title }}
                                         </a>
-                                    </li>
-                                @endforeach
-                            </ul>
+                                    @endforeach
+                                </div>
+                            </div>
                             <div class="card-body">
                                 <div class="form-group">
                                     <label>{{ translation('Title') }}</label>
